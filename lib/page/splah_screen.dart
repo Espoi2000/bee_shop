63a21f457b6a46d991d7e570d9901d086630d47f
leudjeu prince <leudjeuprince@gmail.com>
2024-12-09 23:35:53 +0100

    start translate language

--- lib/page/splah_screen.dart
+++ lib/page/splah_screen.dart
@@ -1,5 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+// import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 @RoutePage()
 class SplashScreen extends StatefulWidget {
@@ -12,12 +13,13 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
+    // var trans = AppLocalizations.of(context);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Splash Screen'),
       ),
-      body: const Center(
-        child: Text('Splash Screen'),
+      body: Center(
+        child: Text("trans!.helloWorld"),
       ),
     );
   }
--- lib/shared/application.dart
+++ lib/shared/application.dart
@@ -1,5 +1,6 @@
 import 'package:beep_shop/router/router.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class Application extends StatefulWidget {
   const Application({super.key});
@@ -13,13 +14,15 @@ class _ApplicationState extends State<Application> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      routerConfig: _appRouter.config(),
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-    );
+        routerConfig: _appRouter.config(),
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        locale: const Locale("fr"),
+        supportedLocales: AppLocalizations.supportedLocales);
     ;
   }
 }
